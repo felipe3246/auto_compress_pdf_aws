d93319b3d3286b5ac097b36a433abf6927f20061
Felipe Gonzales <felipemgonzales@hotmail.com>
2024-05-29 15:18:24 -0300

    change inotify to send sns message instead of sqs

--- etc/systemd/system/sei-file-compressor.d/sei_file_compressor.sh
+++ etc/systemd/system/sei-file-compressor.d/sei_file_compressor.sh
@@ -5,6 +5,6 @@ inotifywait -m $SEI_WATCH_DIR -e create -e moved_to
 while read dir action file; do
     echo "The file '$file' appeared in directory '$dir' via '$action'"  | tee -a $SEI_LOGFILE
     
-    # push a message to SQS QUEUE 
-    aws sqs send-message --queue-url $SEI_SQS_QUEUE_URL --message-body "$file"
+    # push a message to SNS TOPIC
+    aws sns publish --topic-arn $SEI_SNS_ARN --message '{"full_file_name": "'"$file"'"}' 
 done
